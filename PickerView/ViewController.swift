325ab66c27dac85f0dcffc2ef35f5ce49ffcff7e
Alexey Ostroverkhov <a.ostroverkhov@spider.ru>
2018-09-19 10:04:41 +0300

    init commit

---
+++ PickerView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PickerView
+//
+//  Created by a.ostroverkhov on 19.09.2018.
+//  Copyright © 2018 a.ostroverkhov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
